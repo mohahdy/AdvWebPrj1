638a6c6420ffb41523fd6fdad6a8ff5cc4d8c2fa
mohahdy <mohahdyc@hotmail.com>
2021-11-19 21:35:57 +0200

    Fixed search issue when a book has no thumbnail.

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -15,12 +15,13 @@ class Book extends React.Component{
 render(){
  //console.log(`inside Book Component this.props.book is ${JSON.stringify(this.props.book)}`)
   const id=this.props.book.id;
+  const thumbnail = (this.props.book.imageLinks!==undefined)?this.props.book.imageLinks.thumbnail:"";
   return(
    
   					  <li key={id}>
                         <div className="book">
                           <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}></div>
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${thumbnail})` }}></div>
                             <div className="book-shelf-changer">
             <select value={this.state.value} onChange={(event)=>this.handleChange(this.props.book,event)}>
                                 <option value="move" disabled>Move to...</option>
